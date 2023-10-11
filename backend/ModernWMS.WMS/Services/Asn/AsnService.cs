64eac656e5390a1fe8f3186210a07e71e6f840eb
amo <513580005@qq.com>
2023-10-11 15:00:22 +0800

    asn PageAsync

--- backend/ModernWMS.WMS/Services/Asn/AsnService.cs
+++ backend/ModernWMS.WMS/Services/Asn/AsnService.cs
@@ -67,7 +67,12 @@ namespace ModernWMS.WMS.Services
                 });
             }
             Byte asn_status = 255;
-            if (pageSearch.sqlTitle.ToLower().Contains("asn_status"))
+            bool isShowAllData = false;
+            if (pageSearch.sqlTitle.ToLower().Contains("asn_status:-1"))
+            {
+                isShowAllData = true;
+            }
+            else if (pageSearch.sqlTitle.ToLower().Contains("asn_status"))
             {
                 asn_status = Convert.ToByte(pageSearch.sqlTitle.Trim().ToLower().Replace("asn_status","").Replace("：", "").Replace(":", "").Replace("=", ""));
                 asn_status = asn_status.Equals(4) ? (Byte)255 : asn_status;
@@ -79,7 +84,7 @@ namespace ModernWMS.WMS.Services
                         join p in Spus.AsNoTracking() on m.spu_id equals p.id
                         join k in Skus.AsNoTracking() on m.sku_id equals k.id
                         where m.tenant_id == currentUser.tenant_id
-                        && (asn_status == -1 || asn_status == 255 || m.asn_status == asn_status)
+                        && (isShowAllData == true || asn_status == 255 || m.asn_status == asn_status)
                         select new AsnViewModel
                         {
                             id = m.id,
