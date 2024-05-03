9ee781afb18d9ff3a15a462ac830995a32c67cdb
niugeng-gatech <niugeng.gatech@gatech.edu>
2024-05-02 23:55:32 -0700

    UseIISIntegration

--- backend/ModernWMS/Program.cs
+++ backend/ModernWMS/Program.cs
@@ -34,7 +34,8 @@ namespace ModernWMS
                     webBuilder
                     .UseUrls("http://*:5555")
                     .UseStartup<Startup>()
-                    .UseKestrel(opt => opt.Limits.MaxRequestBodySize = null);
+                    .UseIISIntegration();
+                    //.UseKestrel(opt => opt.Limits.MaxRequestBodySize = null);
                 }).ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
